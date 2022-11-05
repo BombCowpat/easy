4847cd09fc7d22c105ed75a737e5bd35e762f05d
chenyanfei <15873624283@163.com>
2022-11-05 11:18:30 +0800

    feat: element-plus

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,14 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
+      <el-row>
+        <el-button>Default</el-button>
+        <el-button type="primary">Primary</el-button>
+        <el-button type="success">Success</el-button>
+        <el-button type="info">Info</el-button>
+        <el-button type="warning">Warning</el-button>
+        <el-button type="danger">Danger</el-button>
+      </el-row>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
